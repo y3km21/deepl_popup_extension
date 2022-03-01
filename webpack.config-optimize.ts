8687930a38397b553f1a2e56da675b8de6e70c0e
y3km21 <y3km21@gmail.com>
2022-03-01 13:11:33 +0900

    Remove elm-hot-webpack-loader

--- webpack.config-optimize.ts
+++ webpack.config-optimize.ts
@@ -5,166 +5,162 @@ import * as HtmlWebpackPlugin from "html-webpack-plugin";
 import * as CopyPlugin from "copy-webpack-plugin";
 
 /************************************************************
- * 
+ *
  * Plugins
- * 
+ *
  ************************************************************/
 
-const htmlWebpackPluginArray = (fileNameArray: string[]): (
-    ((this: webpack.Compiler, compiler: webpack.Compiler) => void)
-    | webpack.WebpackPluginInstance
+const htmlWebpackPluginArray = (
+  fileNameArray: string[]
+): (
+  | ((this: webpack.Compiler, compiler: webpack.Compiler) => void)
+  | webpack.WebpackPluginInstance
 )[] => {
-    return fileNameArray.map(fileName => new HtmlWebpackPlugin({
+  return fileNameArray.map(
+    (fileName) =>
+      new HtmlWebpackPlugin({
         filename: fileName + ".html",
         chunks: [fileName],
         title: fileName,
-        template: "src/index.html"
-    }));
+        template: "src/index.html",
+      })
+  );
 };
 
 const copyManifestJson: CopyPlugin = new CopyPlugin({
-    patterns: [
-        {
-            from: path.resolve(__dirname, "src", "manifest.json"),
-            to: path.resolve(__dirname,
-                "prod", "manifest.json")
-        }
-    ]
-})
+  patterns: [
+    {
+      from: path.resolve(__dirname, "src", "manifest.json"),
+      to: path.resolve(__dirname, "prod", "manifest.json"),
+    },
+  ],
+});
 
-let copyIcons: (sizenums: number[], outdir: string) => CopyPlugin = (sizenums, outdir) => {
-    let patterns = sizenums.map((val) => {
-        return {
-            from: path.resolve(__dirname, "src", "icons", `icon${val}.png`),
-            to: path.resolve(__dirname, outdir, `icon${val}.png`)
-        }
-    })
+let copyIcons: (sizenums: number[], outdir: string) => CopyPlugin = (
+  sizenums,
+  outdir
+) => {
+  let patterns = sizenums.map((val) => {
+    return {
+      from: path.resolve(__dirname, "src", "icons", `icon${val}.png`),
+      to: path.resolve(__dirname, outdir, `icon${val}.png`),
+    };
+  });
 
-    return new CopyPlugin({ patterns: patterns })
-}
+  return new CopyPlugin({ patterns: patterns });
+};
 let iconsSize = [16, 48, 128];
 
-
 const plugins: (
-    ((this: webpack.Compiler, compiler: webpack.Compiler) => void)
-    | webpack.WebpackPluginInstance
-)[]
-    = htmlWebpackPluginArray(["popup", "options"]).concat(copyManifestJson).concat(copyIcons(iconsSize, "prod"));
+  | ((this: webpack.Compiler, compiler: webpack.Compiler) => void)
+  | webpack.WebpackPluginInstance
+)[] = htmlWebpackPluginArray(["popup", "options"])
+  .concat(copyManifestJson)
+  .concat(copyIcons(iconsSize, "prod"));
 
 /************************************************************
- * 
+ *
  * devServer
- * 
+ *
  ************************************************************/
 
 const devServer: webpackDevServer.Configuration = {
-    hot: true,
-    index: "options.html",
-    contentBase: path.join(__dirname, "prod"),
-    compress: true,
-    port: 8080,
+  hot: true,
+  index: "options.html",
+  contentBase: path.join(__dirname, "prod"),
+  compress: true,
+  port: 8080,
 };
 
 /************************************************************
- * 
+ *
  * Module
- * 
+ *
  ************************************************************/
 // Rules
 const ts: webpack.RuleSetRule = {
-    test: /\.tsx?$/,
-    use: [
-        {
-            loader: "ts-loader",
-            options: {
-                transpileOnly: true,
-            }
-        }
-    ],
-    exclude: [/node_modules/],
+  test: /\.tsx?$/,
+  use: [
+    {
+      loader: "ts-loader",
+      options: {
+        transpileOnly: true,
+      },
+    },
+  ],
+  exclude: [/node_modules/],
 };
-
 
 const scss: webpack.RuleSetRule = {
-    test: /\.s[ac]ss$/i,
-    use: [
-        // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader",
-        // Compiles Sass to CSS
-        "sass-loader",
-    ],
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    "style-loader",
+    // Translates CSS into CommonJS
+    "css-loader",
+    // Compiles Sass to CSS
+    "sass-loader",
+  ],
 };
 
-var elmSource = path.resolve(__dirname, "src", "options",);
+var elmSource = path.resolve(__dirname, "src", "options");
 
 const elm: webpack.RuleSetRule = {
-    test: /\.elm$/,
-    exclude: [/elm-stuff/, /node_modules/],
-    use: [
-        {
-            loader: 'elm-hot-webpack-loader',
-            options: { debug: false }
-        },
-        {
-            loader: 'elm-webpack-loader',
-            options: {
-                cwd: elmSource,
-                optimize: true
-            },
-        }
-    ]
-}
+  test: /\.elm$/,
+  exclude: [/elm-stuff/, /node_modules/],
+  use: [
+    {
+      loader: "elm-webpack-loader",
+      options: {
+        cwd: elmSource,
+        optimize: true,
+      },
+    },
+  ],
+};
 
-
-const rules: webpack.RuleSetRule[] = [
-    ts,
-    scss,
-    elm,
-];
+const rules: webpack.RuleSetRule[] = [ts, scss, elm];
 
 // Module
 const moduleObj: webpack.ModuleOptions = {
-    rules: rules
+  rules: rules,
 };
 
 /************************************************************
- * 
+ *
  * Resolve
- * 
+ *
  ************************************************************/
 
 const resolve: webpack.ResolveOptions = {
-    extensions: [".tsx", ".ts", ".js"],
-    fallback: {
-        "path": require.resolve("path-browserify"),
-    }
-}
+  extensions: [".tsx", ".ts", ".js"],
+  fallback: {
+    path: require.resolve("path-browserify"),
+  },
+};
 
 /************************************************************
- * 
+ *
  * Config
- * 
+ *
  ************************************************************/
 
 const config: webpack.Configuration = {
-    entry: {
-        popup: "./src/popup/bootstrap.ts",
-        options: "./src/options/bootstrap.ts",
-        background: "./src/background/index.ts",
-
-    },
-    devtool: "inline-source-map",
-    output: {
-        path: path.resolve(__dirname, "prod"),
-        filename: "[name].js",
-    },
-    mode: "production",
-    plugins: plugins,
-    devServer: devServer,
-    module: moduleObj,
-    resolve: resolve,
+  entry: {
+    popup: "./src/popup/bootstrap.ts",
+    options: "./src/options/bootstrap.ts",
+    background: "./src/background/index.ts",
+  },
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "prod"),
+    filename: "[name].js",
+  },
+  mode: "production",
+  plugins: plugins,
+  devServer: devServer,
+  module: moduleObj,
+  resolve: resolve,
 };
 
 export default config;
